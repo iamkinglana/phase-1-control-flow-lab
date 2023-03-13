a9c7cbf7d4063a2041318642404a77b0902a7558
+ <+>
2023-03-14 00:31:35 +0300

    Control Flow Lab

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  
+  
+  if (ride <=400) {
+    return ("This one is on me!");
+
+  } else if( ride > 2000 && ride <=2500){
+    return ("I will gladly take your thirty bucks.");
+
+  }else if (ride > 2500){
+    return ("No can do.");
+  }
+     
 }
 
-function ternaryCheckCity(){
+
+
+function ternaryCheckCity(city){
   // Write your code here!
+  let Check = city== "NYC"
+  let ans = ""
+  
+  Check ? ans ="Ok, sounds good." : ans = "No go.";
+  return ans;
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye."
+}
 }
